3bb0ac2183dbb2faab917268ba1b78ea07cec536
Michael <michael.sim132@gmail.com>
2025-08-11 03:06:53 +0000

    fix cors issue with new endpoint

--- ratedrps-server/src/main/java/com/michaeldavidsim/ratedrps_server/controller/HealthCheckController.java
+++ ratedrps-server/src/main/java/com/michaeldavidsim/ratedrps_server/controller/HealthCheckController.java
@@ -2,10 +2,12 @@ package com.michaeldavidsim.ratedrps_server.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
+@CrossOrigin(origins = {"https://ratedrps.vercel.app", "http://localhost:5173"})
 public class HealthCheckController {
     
     @GetMapping("/api/health_check")
